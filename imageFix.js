8820f0751ab1b465dc584653d226551acfda2d92
CRSAds <yuri@crsadvertising.com>
2025-06-03 09:47:57 +0200

    Update imageFix.js

--- imageFix.js
+++ imageFix.js
@@ -1,18 +1,20 @@
 export function reloadImages(section) {
+  // 1. Trigger lazyload door scroll event te simuleren
+  window.dispatchEvent(new Event('scroll'));
+
+  // 2. Forceer herladen van zichtbare afbeeldingen
   const images = section.querySelectorAll('img');
   images.forEach(img => {
-    const src = img.src;
-    img.src = '';
-    img.src = src;
-  });
-}
-
-export default function setupImageReloading() {
-  document.querySelectorAll('img').forEach(img => {
-    const src = img.getAttribute('src');
-    if (src) {
-      const preload = new Image();
-      preload.src = src;
+    if (img.dataset.src && !img.src.includes(img.dataset.src)) {
+      img.src = img.dataset.src;
+    } else {
+      const src = img.src;
+      img.src = '';
+      img.src = src;
     }
+
+    // 3. Verwijder mogelijk opacity:0 styles die lazyload blokken
+    img.style.opacity = '1';
+    img.style.visibility = 'visible';
   });
 }
