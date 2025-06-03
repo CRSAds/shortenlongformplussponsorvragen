ff45ab8ccacbe931e5a67690f7a7b0720a9af61f
CRSAds <yuri@crsadvertising.com>
2025-06-03 13:23:03 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // main.js
 import initFlow from './initFlow.js';
 import setupFormSubmit from './formSubmit.js';
-import { reloadImages } from './imageFix.js';
+import { setupImageFix } from './imageFix.js';
 
 // Start logica
 setupImageFix();
