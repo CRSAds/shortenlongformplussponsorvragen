cecd1c60f97caad6ffe29ff3d662227b846cf65f
CRSAds <yuri@crsadvertising.com>
2025-06-05 15:37:58 +0200

    Update initFlow.js

--- initFlow.js
+++ initFlow.js
@@ -75,11 +75,16 @@ export default function initFlow() {
         const next = steps[index + 1];
         const upcomingCoregs = steps.slice(index + 1).filter(s => s.classList.contains('coreg-section'));
 
-        if (upcomingCoregs.length === 0) {
-          if (longFormCampaigns.length > 0 && longFormSection) {
-            longFormSection.style.display = 'block';
-            reloadImages(longFormSection);
-          }
+       if (upcomingCoregs.length === 0 && longFormSection) {
+  if (longFormCampaigns.length === 0) {
+    // Geen long form nodig: verberg
+    longFormSection.style.display = 'none';
+  } else {
+    // Wel long form nodig: toon
+    longFormSection.style.display = 'block';
+    reloadImages(longFormSection);
+  }
+}
           // Als er geen long form campagnes zijn, dan gewoon door naar volgende sectie
           else if (next) {
             next.style.display = 'block';
