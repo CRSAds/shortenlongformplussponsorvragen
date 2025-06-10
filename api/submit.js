2cd35b38e6ecfaaa0cf03752414eff097550cf1e
CRSAds <yuri@crsadvertising.com>
2025-06-10 18:29:43 +0200

    Update submit.js

--- api/submit.js
+++ api/submit.js
@@ -39,29 +39,32 @@ export default async function handler(req, res) {
     const dob = `${dob_day?.padStart(2, '0')}/${dob_month?.padStart(2, '0')}/${dob_year}`;
     const ipaddress = req.headers['x-forwarded-for'] || req.socket?.remoteAddress || '';
     const optindate = new Date().toISOString().split('.')[0] + '+0000';
-    const campagne_url = req.headers.referer || '';
 
-const params = new URLSearchParams({
-  cid: String(cid),
-  sid: String(sid),
-  f_2_title: gender || '',
-  f_3_firstname: firstname || '',
-  f_4_lastname: lastname || '',
-  f_1_email: email || '',
-  f_5_dob: dob || '',
-  f_11_postcode: postcode || '',
-  f_6_address1: straat || '',
-  f_7_address2: huisnummer || '',
-  f_8_address3: '', // optioneel extra veld
-  f_9_towncity: woonplaats || '',
-  f_12_phone1: telefoon || '',
-  f_17_ipaddress: ipaddress,
-  f_55_optindate: optindate,
-  f_1322_transaction_id: t_id || '',
-  f_2014_coreg_answer: f_2014_coreg_answer || '',
-  f_1453_campagne_url: campagne_url,
-  f_2047_EM_CO_sponsors: req.body.f_2047_EM_CO_sponsors || ''
-});
+    // ✅ Campagne URL nu correct zonder query params:
+    const campagne_url_full = req.headers.referer || '';
+    const campagne_url = campagne_url_full.split('?')[0];
+
+    const params = new URLSearchParams({
+      cid: String(cid),
+      sid: String(sid),
+      f_2_title: gender || '',
+      f_3_firstname: firstname || '',
+      f_4_lastname: lastname || '',
+      f_1_email: email || '',
+      f_5_dob: dob || '',
+      f_11_postcode: postcode || '',
+      f_6_address1: straat || '',
+      f_7_address2: huisnummer || '',
+      f_8_address3: '', // optioneel extra veld
+      f_9_towncity: woonplaats || '',
+      f_12_phone1: telefoon || '',
+      f_17_ipaddress: ipaddress,
+      f_55_optindate: optindate,
+      f_1322_transaction_id: t_id || '',
+      f_2014_coreg_answer: f_2014_coreg_answer || '',
+      f_1453_campagne_url: campagne_url,
+      f_2047_EM_CO_sponsors: req.body.f_2047_EM_CO_sponsors || ''
+    });
 
     const response = await fetch('https://crsadvertising.databowl.com/api/v1/lead', {
       method: 'POST',
