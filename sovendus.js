59c49a730a9e5a1e5bb18d2834a467f56a51226a
CRSAds <yuri@crsadvertising.com>
2025-06-12 11:09:42 +0000

    Update sovendus.js

--- sovendus.js
+++ sovendus.js
@@ -1,10 +1,1 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const showBtn = document.getElementById('show-sovendus-btn');
-  const sovendusContainer = document.getElementById('sovendus-container-1');
 
-  if (showBtn && sovendusContainer) {
-    showBtn.addEventListener('click', function() {
-      sovendusContainer.style.display = 'block';
-    });
-  }
-});
